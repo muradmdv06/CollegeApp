9e548905e49bea46ce59669e57fa896620c85e13
muradmdv06 <muradmammadov066@gmail.com>
2025-02-28 15:45:29 +0400

    Update CollegeDBContext.cs

--- CollegeApp/Data/CollegeDBContext.cs
+++ CollegeApp/Data/CollegeDBContext.cs
@@ -1,4 +1,4 @@
-﻿using CollegeApp.Data.Config;
+using CollegeApp.Data.Config;
 using Microsoft.EntityFrameworkCore;
 
 namespace CollegeApp.Data
@@ -10,13 +10,21 @@ namespace CollegeApp.Data
             
         }
         public DbSet<Student> Students { get; set; }
-        public DbSet<Department> Departments { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<RolePrivelege> RolePriveleges { get; set; }
+        public DbSet<UserRoleMapping> UserRoleMappings { get; set; }
+
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             //Table 1
             modelBuilder.ApplyConfiguration(new StudentConfig());
-            modelBuilder.ApplyConfiguration(new DepartmentConfig());
+            modelBuilder.ApplyConfiguration(new UserConfig());
+            modelBuilder.ApplyConfiguration(new RoleConfig());
+            modelBuilder.ApplyConfiguration(new RolePrivelegeConfig());
+            modelBuilder.ApplyConfiguration(new UserRoleMappingConfig());
+
 
 
 
