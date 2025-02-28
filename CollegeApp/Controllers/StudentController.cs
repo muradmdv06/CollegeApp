fc13321c8738771371c6ebb40619c4192a638990
muradmdv06 <muradmammadov066@gmail.com>
2025-02-28 15:54:02 +0400

    Update StudentController.cs

--- CollegeApp/Controllers/StudentController.cs
+++ CollegeApp/Controllers/StudentController.cs
@@ -3,21 +3,25 @@ using CollegeApp.Data;
 using CollegeApp.Data.Repo;
 using CollegeApp.Data.Repository;
 using CollegeApp.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System.Collections.Generic;
+using System.Net;
 using System.Threading.Tasks;
 
 namespace CollegeApp.Controllers
 {
     [Route("api/students")]
     [ApiController]
+    //[Authorize(Roles ="Superadmin,Admin")]
     public class StudentController : ControllerBase
     {
         private readonly ILogger<StudentController> _logger;
         private readonly IMapper _mapper;
         private readonly ICollegeRepository<Student> _studentRepository;
+        private APIResponse _apiResponse;
 
         public StudentController(ILogger<StudentController> logger, IMapper mapper,
             ICollegeRepository<Student> studentRepository)
@@ -25,14 +29,20 @@ namespace CollegeApp.Controllers
             _logger = logger;
             _mapper = mapper;
             _studentRepository = studentRepository;
+            _apiResponse= new ();
         }
 
         [HttpGet]
+        //[AllowAnonymous]
         public async Task<ActionResult<IEnumerable<StudentDTO>>> GetStudents()
         {
             _logger.LogInformation("Fetching all students.");
             var students = await _studentRepository.GetAllAsync();
-            return Ok(_mapper.Map<IEnumerable<StudentDTO>>(students));
+
+            _apiResponse.data=_mapper.Map<List<StudentDTO>>(students);
+            _apiResponse.Status = true;
+            _apiResponse.StatusCode=HttpStatusCode.OK;
+            return Ok(_apiResponse);
         }
 
         [HttpGet("{id:int}", Name = "GetStudentById")]
@@ -41,9 +51,12 @@ namespace CollegeApp.Controllers
             if (id <= 0) return BadRequest("Invalid student ID.");
 
             var student = await _studentRepository.GetAsync(student => student.Id == id);
+            _apiResponse.data = _mapper.Map<StudentDTO>(student);
+            _apiResponse.Status = true;
+            _apiResponse.StatusCode = HttpStatusCode.OK;
             if (student == null) return NotFound($"Student with ID {id} not found.");
 
-            return Ok(_mapper.Map<StudentDTO>(student));
+            return Ok(_apiResponse);
         }
 
         [HttpGet("name/{name}", Name = "GetStudentByName")]
@@ -52,9 +65,12 @@ namespace CollegeApp.Controllers
             if (string.IsNullOrWhiteSpace(name)) return BadRequest("Name cannot be empty.");
 
             var student = await _studentRepository.GetAsync(student => student.StudentName.ToLower().Contains(name));
+            _apiResponse.data = _mapper.Map<StudentDTO>(student);
+            _apiResponse.Status = true;
+            _apiResponse.StatusCode = HttpStatusCode.OK;
             if (student == null) return NotFound($"Student with name {name} not found.");
 
-            return Ok(_mapper.Map<StudentDTO>(student));
+            return Ok(_apiResponse);
         }
 
         [HttpDelete("{id:int}")]
@@ -65,8 +81,12 @@ namespace CollegeApp.Controllers
             var student = await _studentRepository.GetAsync(student => student.Id == id);
             if (student == null) return NotFound($"Student with ID {id} not found.");
 
-            
-            return Ok(true);
+            _apiResponse.data = true;
+            _apiResponse.Status = true;
+            _apiResponse.StatusCode = HttpStatusCode.OK;
+
+
+            return Ok(_apiResponse);
         }
 
         [HttpPut("{id:int}")]
@@ -94,7 +114,11 @@ namespace CollegeApp.Controllers
 
             dto.Id = studentAfrerCreation.Id;
 
-            return CreatedAtRoute("GetStudentById", new { id = student.Id }, _mapper.Map<StudentDTO>(student));
+            _apiResponse.data = dto;
+            _apiResponse.Status = true;
+            _apiResponse.StatusCode = HttpStatusCode.OK;
+
+            return CreatedAtRoute("GetStudentById", new { id = student.Id }, _apiResponse);
         }
 
         [HttpPut("update")]
