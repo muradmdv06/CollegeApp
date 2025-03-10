e0b0085de632960baf72b730d199f3bdaa4902ed
muradmdv06 <muradmammadov066@gmail.com>
2025-03-11 00:26:34 +0400

    Update AutoMapperConfig.cs

--- CollegeApp/Configurations/AutoMapperConfig.cs
+++ CollegeApp/Configurations/AutoMapperConfig.cs
@@ -6,17 +6,17 @@ namespace CollegeApp.Configurations
 {
     public class AutoMapperConfig : Profile
     {
-        public AutoMapperConfig() {
+        public AutoMapperConfig()
+        {
             //CreateMap<Student, StudentDTO>();
             //CreateMap<StudentDTO, Student>();
 
             //CreateMap<StudentDTO, Student>().ForMember(n=>n.StudentName,opt=>opt.MapFrom(x=>x.Name)).ReverseMap();
             //CreateMap<StudentDTO, Student>().ReverseMap().ForMember(n => n.StudentName, opt => opt.Ignore());
-            CreateMap<StudentDTO, Student>().ReverseMap().AddTransform<string>(n=>string.IsNullOrEmpty(n)?"No address found": n);
+            CreateMap<StudentDTO, Student>().ReverseMap().AddTransform<string>(n => string.IsNullOrEmpty(n) ? "No address found" : n);
             CreateMap<RoleDTO, Role>().ReverseMap();
             CreateMap<RolePrivilegeDTO, RolePrivilege>().ReverseMap();
             CreateMap<UserDTO, User>().ReverseMap();
-
 
             //CreateMap<StudentDTO, Student>().ReverseMap();
         }
